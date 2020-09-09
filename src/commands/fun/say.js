289c356e7ee7a3acac068167b3955769269d99b1
unknown <mistermelphi@outlook.es>
2020-09-08 22:58:00 -0400

    Comando say agregado

---
+++ src/commands/fun/say.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: 'say',
+    description: 'Repite tu texto',
+    usage: 'say <texto>',
+    aliases: [],
+    categoria: 'Diversión',
+    execute(message, client, args) {
+
+    if(!args[0]) {
+        return message.channel.send('Debes escribir un Mensaje.')
+    }
+    
+    message.channel.send(args.join(' '), { disableMentions: 'all' })
+}
+}
