014e57043c16aac4a5d4963d373242f1afe03f0e
unknown <mistermelphi@outlook.es>
2020-09-08 23:31:27 -0400

    Comando avatar Agregado

--- src/commands/utility/help.js
+++ src/commands/utility/help.js
@@ -11,19 +11,28 @@ module.exports = {
     if(!args[0]) {
         const embed = new Discord.MessageEmbed()
         .setDescription(`!Hola ${message.member.displayName}! Mi nombre es Abismo, pero puedes llamarme como quieras. Yo soy un bot creado para entretenerte y ayudar a tu Server.\nPuedes ver toda la información de un comando con \`abyss!help <comando>\`\n\nTotal de Comandos: ${client.commands.size}`)
-        .addField('Comandos de Información', '`help`')
+        .addField('Comandos de Información', '`help` `avatar` `snipe`')
+        .addField('Comandos de Diversión', '`say`')
         .setColor('RANDOM')
-        message.channel.send(embed)
+       return message.channel.send(embed)
     }
 
-    const cmd = args[0]
+try {
 
     const embed2 = new Discord.MessageEmbed()
-    .addField('Nombre', client.commands.get(cmd).name)
-    .addField('Alias', client.commands.get(cmd).aliases ? client.commands.get(cmd).aliases.join(' ') : 'No tiene alias.')
-    .addField('Usage', client.commands.get(cmd).usage)
-    .addField('Categoria', client.commands.get(cmd).categoria)
+    .addField('Nombre', client.commands.get(args[0]).name)
+    .addField('Alias', client.commands.get(args[0]).aliases[0] ? client.commands.get(args[0]).aliases.join(' ') : 'No tiene alias.')
+    .addField('Usage', client.commands.get(args[0]).usage)
+    .addField('Categoria', client.commands.get(args[0]).categoria)
     message.channel.send(embed2)
+
+} catch(err) {
+
+    return message.channel.send('Ups... El comando Introducido no coincide con ningun comando, o lo escribio en Mayusculas.')
+
+}
+
+    
 
 }
 }
