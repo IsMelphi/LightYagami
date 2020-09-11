cc9c96b7ff6a703f51a240fca04b06426a4d24be
unknown <mistermelphi@outlook.es>
2020-09-10 23:17:10 -0400

    Pequeños Cambios

---
+++ src/commands/utility/serverinfo.js
@@ -0,0 +1,25 @@
+module.exports = {
+    name: 'serverinfo',
+    description: 'Información del Servidor',
+    usage: 'serverinfo',
+    aliases: [],
+    categoria: 'Información',
+    execute(message, client, args) {
+
+    const Discord = require('discord.js')
+    const { Flags, Features }  = require('../../utils/objetos')
+
+    const embed = new Discord.MessageEmbed()
+    .addField('Creador', `**Tag:** ${message.guild.owner.user.username}\n**ID:** ${message.guild.owner.user.id}`)
+    .addField('\<a:NitroNitroso:753722285621248010>Boost Estadisticas', `**Numero de Boost:** ${message.guild.premiumSubscriptionCount}\n**Nivel de Boost:** ${message.guild.premiumTier}`)
+
+    if(message.guild.features) {
+        embed.addField('Features', `\`\`\`\n${message.guild.features ? message.guild.features.map(x => Features[x]).join(', ') : 'No tiene'}\n\`\`\``)
+    }
+
+    embed.addField('Servidor', Flags[message.guild.region])
+    .setColor('RANDOM')
+    message.channel.send(embed)
+
+    }
+}
---
+++ src/utils/objetos.js
@@ -0,0 +1,36 @@
+module.exports = {
+    Badges: {
+        /* Aqui irian las badges en un Futuro */
+    },
+    Flags: {
+        /* Banderas dependiendo del Servidor */
+        'us-west' : ':flag_us: Costa Oeste de los Estados Unidos',
+        'us-east' : ':flag_us: Costa Este de los Estados Unidos',
+        'us-central' : ':flag_us: Centro de Estados Unidos ',
+        'us-south' : 'flag_us: Sur de Estados Unidos',
+        'singapore' : ':flag_si: Singapur', 
+        'southafrica' : ':flag_za: Sudáfrica',
+        'sydney' : ':flag_hm: Sidney',
+        'europe' : ':flag_eu: Europa',
+        'brazil' : ':flag_br: Brazil',
+        'hongkong' : ':flag_hk: Hong Kong',
+        'russia' : ':flag_ru: Rusia',
+        'japan' : ':flag_jp: Japan',
+        'india' : ':flag_in: India'
+    },
+    Features: {
+        'ANIMATED_ICON' : 'Icono Animado',
+        'BANNER' : 'Banner',
+        'COMMERCE' : 'Comercio',
+        'COMMUNITY' : 'Comunidad',
+        'DISCOVERABLE' : 'Descubrirle',
+        'FEATURABLE' : '',
+        'INVITE_SPLASH' : 'Splash de Invitación',
+        'NEWS' : 'Noticias',
+        'PARTNERED' : 'Partner',
+        'VANITY_URL' : 'URL Personalizable',
+        'VERIFIED' : 'Verificado',
+        'VIP_REGIONS' : 'Región VIP',
+        'WELCOME_SCREE_ENABLED' : 'Pantalla de Bievenida'
+    }
+}
