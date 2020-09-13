a48658a5f35845d6319883d3a076b4c0fdedba78
unknown <mistermelphi@outlook.es>
2020-09-12 21:24:22 -0400

    Ahora?

--- src/commands/fun/chat.js
+++ src/commands/fun/chat.js
@@ -7,12 +7,15 @@ module.exports = {
     execute(message, client, args) {
 
     const Discord = require('discord.js')
-    const mensajes = client.chat.get(message.guild.id)
+    const chat = client.chat.get(message.guild.id)
     const Hora = require('../../utils/Utils').hora()
 
     if(!mensajes) return message.channel.send(new Discord.MessageEmbed().setDescription('No hay mensajes en este Servidor.'))
+    
+    const mensaje = chat.map(mensaje => mensaje.mensaje)
+    const autor = chat.map(autor => autor.autor)
 
-    message.channel.send(`[${Hora}][${mensajes.autor}]: ${mensajes.mensaje}`, { code: 'ini' })
+    message.channel.send(`[${Hora}][${autor}]: ${mensaje}`, { code: 'ini' })
 
     }
 }
