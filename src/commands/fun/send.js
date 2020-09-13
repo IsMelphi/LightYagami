ef1943311ba0d449f1526da64f28e4c1fcb8cbef
unknown <mistermelphi@outlook.es>
2020-09-12 21:13:40 -0400

    Pruebas

--- src/commands/fun/send.js
+++ src/commands/fun/send.js
@@ -7,15 +7,13 @@ module.exports = {
     execute(message, client, args) {
 
     const Discord = require('discord.js')
-    const links = args.join(' ').match(/[https://discord.gg]/ig)
-
 
     if(!args[0]) return message.channel.send(new Discord.MessageEmbed().setDescription('Que mensajes enviaras?'))
-    if(links) return message.channel.send(new Discord.MessageEmbed().setDescription('Links :('))
 
     const Objeto = { autor: message.author.tag, mensaje: args.join(' ') }
 
-    client.chat.set(message.guild.id, Objeto)
+    if(!client.chat.has(message.guild.id)) client.chat.set(message.guild.id, [])
+    client.chat.get(message.guild.id).push(Objeto)
 
     message.channel.send(new Discord.MessageEmbed().setDescription('Mensaje Enviado'))
 
