9ed25687f4de9b031c38ef1eb4be52de0da98530
unknown <mistermelphi@outlook.es>
2020-09-13 13:46:18 -0400

    Pequeños cambios

--- src/commands/chat/chat.js
+++ src/commands/chat/chat.js
@@ -8,6 +8,7 @@ module.exports = {
 
     const Discord = require('discord.js')
     const chat = client.chat.get(message.guild.id)
+    const user_random = message.guild.members.cache.random()
 
     if(!chat) return message.channel.send(new Discord.MessageEmbed().setDescription('No hay mensajes en este Servidor.'))
     .then(m => m.delete( { timeout: 4000 } ))
@@ -20,7 +21,7 @@ module.exports = {
     const embed = new Discord.MessageEmbed()
     .setAuthor('Light Chat', client.user.displayAvatarURL({ size: 2048, format: 'png', dynamic: true }))
     .setDescription(`\`\`\`ini\n${chat.map(x => `[${x.hora}][${x.autor}] ${x.mensaje}`).reverse().slice(0, 10).join('\n')}\n\`\`\``)
-    .setFooter(`${message.guild.members.cache.random().displayName} Is Typing.`, message.guild.members.cache.random().user.displayAvatarURL({ size: 2048, dynamic: true, format: 'png' }))
+    .setFooter(`${user_random.displayName} Is Typing.`, user_random.user.displayAvatarURL({ size: 2048, dynamic: true, format: 'png' }))
     .setColor('FFD788')
     message.channel.send(embed)
 
