ac6850e847a8dee9917441109d491d1251abab6a
unknown <mistermelphi@outlook.es>
2020-09-13 12:37:09 -0400

    AAAAAAAAAAAAAAAA

--- src/commands/chat/chat.js
+++ src/commands/chat/chat.js
@@ -14,7 +14,7 @@ module.exports = {
 
     const embed = new Discord.MessageEmbed()
     .setAuthor('Light Chat', client.user.displayAvatarURL({ size: 2048, format: 'png', dynamic: true }))
-    .setDescription(`\`\`\`ini\n${chat.map(x => `[${x.hora}][${x.autor}] ${x.mensaje}`).reverse().slice(3).join('\n')}\n\`\`\``)
+    .setDescription(`\`\`\`ini\n${chat.map(x => `[${x.hora}][${x.autor}] ${x.mensaje}`).reverse().slice(0, 10).join('\n')}\n\`\`\``)
     .setImage('https://img-17.ccm2.net/gpLveMOXJfvAZNBLlK3913vZl_w=/1634x/55e95dd9db6c4123b444fe070e9ef5d0/ccm-faq/Teclado_2.png')
     .setColor('FFD788')
     message.channel.send(embed)
