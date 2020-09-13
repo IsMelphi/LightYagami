66c52b52100acace29c5a6db586d0c9baf391d51
unknown <mistermelphi@outlook.es>
2020-09-12 23:46:29 -0400

    Pequeños cambios

--- src/commands/fun/chat.js
+++ src/commands/fun/chat.js
@@ -13,7 +13,7 @@ module.exports = {
     .then(m => m.delete( { timeout: 4000 } ))
 
     const embed = new Discord.MessageEmbed()
-    .setTitle('Light Chat', client.user.displayAvatarURL({ size: 2048, format: 'png', dynamic: true }))
+    .setAuthor('Light Chat', client.user.displayAvatarURL({ size: 2048, format: 'png', dynamic: true }))
     .setDescription(`\`\`\`ini\n${chat.map(x => `[${x.hora}][${x.autor}] ${x.mensaje}`).reverse().join('\n')}\n\`\`\``)
     .setColor('FFD788')
     message.channel.send(embed)
