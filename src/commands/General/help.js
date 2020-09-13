8f96b3a0023769da9387df692db1d8e0869ecd83
unknown <mistermelphi@outlook.es>
2020-09-13 11:04:29 -0400

    Pequeños Cambios

--- src/commands/General/help.js
+++ src/commands/General/help.js
@@ -14,10 +14,10 @@ module.exports = {
     if(!args[0]) {
         const embed = new Discord.MessageEmbed()
         .setDescription(`!Hola ${message.member.displayName}! Mi nombre es Light Yagami, pero puedes llamarme como quieras. Yo soy un bot creado para entretenerte y ayudar a tu Server.\nPuedes ver toda la información de un comando con \`${Prefix}help <comando>\`\n\nTotal de Comandos: ${client.commands.size}`)
-        .addField('Comandos de Información', '`help` `avatar` `snipe` `editsnipe` `serverinfo`')
-        .addField('Comandos de Diversión', '`say` `meme`')
+        .addField('Comandos de Información', '`avatar` `snipe` `editsnipe` `serverinfo`')
+        .addField('Comandos de Diversión', '`meme`')
         .addField('Comandos de Configuración', '`setprefix`')
-        .addField('Light Chat', '`send` `chat`')
+        .addField('Comandos Generales', '`help` `say` `send` `chat`')
         .setColor('RANDOM')
        return message.channel.send(embed)
     }
