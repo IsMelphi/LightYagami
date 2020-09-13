1342f596261f89ea696bdc454ade886b5a6d0f1e
unknown <mistermelphi@outlook.es>
2020-09-12 22:55:55 -0400

    Prueba

--- src/commands/fun/chat.js
+++ src/commands/fun/chat.js
@@ -12,8 +12,8 @@ module.exports = {
 
     if(!chat) return message.channel.send(new Discord.MessageEmbed().setDescription('No hay mensajes en este Servidor.'))
     
-    const mensaje = chat.map(mensaje => mensaje.mensaje).join('\n')
-    const autor = chat.map(autor => autor.autor).join('\n')
+    const mensaje = chat.map(mensaje => mensaje.mensaje).reverse()
+    const autor = chat.map(autor => autor.autor).reverse()
 
     message.channel.send(`[${Hora}][${autor}]: ${mensaje}`, { code: 'ini' })
 
